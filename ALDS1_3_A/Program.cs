72e46aa4a099883ca2a397719119c990eba84689
菅健一郎 <complete1317@hotmail.co.jp>
2017-05-22 16:52:27 +0900

    プロジェクト ファイルを追加します。

---
+++ ALDS1_3_A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALDS1_3_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
